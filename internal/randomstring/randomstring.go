4b8d6a715c75a415790c84e0532e7d076c1d6a12
physicist2018 <physicist2018@yandex.ru>
2024-12-20 14:51:43 +1000

    Пофиксил ошибку с генератором случайных чисел

--- internal/randomstring/randomstring.go
+++ internal/randomstring/randomstring.go
@@ -7,9 +7,10 @@ import (
 
 const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+var rnd = rand.New(rand.NewSource(10))
+
 // RandomString returns a random string of length n.
 func RandomString(n int) string {
-	rand.Seed(10)
 	return RandomStringWithAlphabet(n, alphabet)
 }
 
@@ -17,7 +18,7 @@ func RandomString(n int) string {
 func RandomStringWithAlphabet(n int, alphabet string) string {
 	var b strings.Builder
 	for i := 0; i < n; i++ {
-		b.WriteByte(alphabet[rand.Intn(len(alphabet))])
+		b.WriteByte(alphabet[rnd.Intn(len(alphabet))])
 	}
 	return b.String()
 }
