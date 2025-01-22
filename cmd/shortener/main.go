a88cfb7a04a7be06dd26ac6c59c5543b95cd9164
physicist2018 <physicist2018@yandex.ru>
2025-01-22 11:52:03 +1000

    Add new config option; Add tags to url model; Add gracefull shutdown

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"compress/gzip"
-	"log"
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
 	"math/rand"
@@ -36,19 +41,62 @@ func main() {
 	sugar := logger.Sugar()
 
 	randomStringGenerator := utils.NewRandomString(maxShortURLLength, rand.New(rand.NewSource(time.Now().UnixNano())))
+
 	urlRepo := memory.NewURLRepositoryMap()
 	urlService := url.NewURLService(urlRepo, randomStringGenerator)
 	urlHandler := httphandlers.NewURLHandler(urlService, configuration.BaseURLServer)
 
 	r := chi.NewRouter()
+
+	// Устанавливаем необходимые middlwares
 	r.Use(compressor.CompressionMiddleware(gzip.BestCompression))
 	r.Use(middleware.AllowContentType("text/plain", "application/json", "text/html", "application/x-gzip"))
 	r.Use(httplogger.LoggerMiddleware(sugar))
+
+	// Определяем наши эндпоинты
 	r.Post("/", urlHandler.HandleGenerateShortURL)
 	r.Post("/api/shorten", urlHandler.HandleGenerateShortURLJson)
 	r.Get("/{shortURL}", urlHandler.HandleRedirect)
 
-	if err := http.ListenAndServe(configuration.ServerAddr, r); err != nil {
-		log.Fatal(err)
+	server := http.Server{
+		Addr:    configuration.ServerAddr,
+		Handler: r,
 	}
+
+	// Используем канал для перехвата сигналов завершения
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	// Создаем контекст для управления временем завершения
+	ctx := context.WithoutCancel(context.Background())
+
+	// Используем sync.WaitGroup для ожидания завершения горутин
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// Запуск HTTP-сервера в горутине
+	go func() {
+		defer wg.Done()
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Println("Ошибка при запуске сервера:", err)
+		}
+	}()
+	fmt.Println("\nСервер зaпущен!")
+	// Ожидаем получения сигнала завершения
+	<-stop
+	fmt.Println("\nПолучен сигнал завершения, выключаем сервер...")
+
+	// Создаем контекст с таймаутом для корректного завершения сервера
+	shutdownCtx, cancelShutdown := context.WithTimeout(ctx, 5*time.Second)
+	defer cancelShutdown()
+
+	// Закрытие HTTP-сервера
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		fmt.Println("Ошибка при выключении сервера:", err)
+	}
+
+	// Ожидаем завершения всех горутин
+	wg.Wait()
+
+	fmt.Println("Сервер адекватно выключен.")
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -6,8 +6,9 @@ import (
 )
 
 type Config struct {
-	ServerAddr    string
-	BaseURLServer string
+	ServerAddr      string
+	BaseURLServer   string
+	FileStoragePath string
 }
 
 func (c *Config) Parse() {
@@ -19,12 +20,17 @@ func (c *Config) Parse() {
 	if envBaseURLServer := os.Getenv("BASE_URL"); envBaseURLServer != "" {
 		c.BaseURLServer = envBaseURLServer
 	}
+
+	if envFileStoragePath := os.Getenv("FILE_STORAGE_PATH"); envFileStoragePath != "" {
+		c.FileStoragePath = envFileStoragePath
+	}
 }
 
 func MakeConfig() *Config {
 	cfg := &Config{}
 	flag.StringVar(&cfg.ServerAddr, "a", "localhost:8080", "адрес интерфейса, на котором запускать сервер")
 	flag.StringVar(&cfg.BaseURLServer, "b", "http://localhost:8080", "префикс короткого URL")
+	flag.StringVar(&cfg.FileStoragePath, "f", "dbase.json", "имя файла персистентного хранилища коротких URL")
 
 	return cfg
 }
