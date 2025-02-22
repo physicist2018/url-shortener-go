a1d40b6b429934bc81c182f97b731e8f5b0e19d9
physicist2018 <physicist2018@yandex.ru>
2025-02-22 14:44:24 +1000

    Iter 15 Fix

--- internal/repository/database/postgresdbrepo/linktable.sql
+++ internal/repository/database/postgresdbrepo/linktable.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS `links` (
-    `id` SERIAL PRIMARY KEY,
     `user_id` VARCHAR(36) NOT NULL,
     `short_url` VARCHAR(36) NOT NULL,
     `original_url` VARCHAR(512) NOT NULL UNIQUE,
--- internal/repository/database/sqlitedbrepo/linktable.sql
+++ internal/repository/database/sqlitedbrepo/linktable.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS `links` (
-    `id` INTEGER PRIMARY KEY,
     `user_id` TEXT NOT NULL,
     `short_url` TEXT NOT NULL,
     `original_url` TEXT NOT NULL UNIQUE,
